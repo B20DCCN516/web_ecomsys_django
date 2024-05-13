908dea83f3c6c499b7d4126b7b9420c1e7c6a4dd
thanhphuoc29 <thanhphuocx10@gmail.com>
2024-05-13 15:13:46 +0700

    first update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './layout';
+import Home from './components/home';
+import Login from './components/login';
+import Register from './components/register/register';
+import ProductDetail from './components/detail';
+import Cart from './components/cart';
+import AdminHome from './components/manager/adminHome';
+import EditProduct from './components/manager/editProduct';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Layout></Layout>}>
+              <Route path='' element={<Home></Home>}></Route>
+              <Route path='/login' element={<Login></Login>}></Route>
+              <Route path='/register' element={<Register></Register>}></Route>
+              <Route path='/product/:product_id' element={<ProductDetail></ProductDetail>}></Route>
+              <Route path='/cart' element={<Cart></Cart>}></Route>
+              <Route path='/manager' element={<AdminHome></AdminHome>}></Route>
+              <Route path='/edit' element={<EditProduct></EditProduct>}></Route>
+            </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
